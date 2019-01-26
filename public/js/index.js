5dcccd7a8cecbb6f9f7b0e54c3d728bf091c8d22
gauravkhurana <gauravkhurana154@gmail.com>
2019-01-26 16:17:09 +0530

    added acknowledgment and remove commits

--- public/js/index.js
+++ public/js/index.js
@@ -1,48 +1,23 @@
  
 var socket = io();
 
-// io is the method available to us by above library to initaite the request and when it calls we initiang the req
-// we are making the req from client to server to open up web socket and keep that connection open
-// what we get back is responsible for emit and listen the events
-
 socket.on("connect",function(){
     console.log("Connected to server");
 });
-
-//io on to listen to custom events
-
-//emit let us to register the event 
-//in 1st agrumnet -> we send event name
-//in 2nd argument -> we send custom data  
-
-
-// socket.emit("createEmail",{
-//     to:"gaurav khurana",
-//     text:"Hey. this is from gaurav"
-// })
-
- 
-
-
-//it is not the realistic example 
-//in general client will make send the form with some custom data
-
-// socket.on("newEmail",function(email){
-//     console.log("New email",email);
-// })
 
 
 socket.on("newMessage",function(message){
     console.log("Got it",message)
 })
 
-// socket.emit("createMessageEvent",{
-//     from:"gauravKhurana",
-//     text:"Helloo"
-// })
+//Acknowledgment  
+socket.emit("createMessageEvent",{
+    from:"frank",
+    text:"Hii" 
+},function(msg){
+    console.log(msg);
+})
 
-//disconnect events=which lets u do something when server and client drops
 socket.on("disconnect",function(){
     console.log("Disconnected from server");
 })
-//disconnect is fire when server goes down 
--- server/server.js
+++ server/server.js
@@ -10,87 +10,27 @@ const {generate_message} = require("./utils/message");
 
 const server = http.createServer(app);
 
-//configure the server to also use socketIO
-//we are pass the server that we have to use with our web sockets
 var io = socketIO(server);
-//without this we dont get access to frontend socket library to take care for new user
 
 
-//when we integrate  socket io we accept 2 things 
-//we accept web socket connection 
-//and js library
-
-//what we get back web socket server
-//emmiting or listening the events
-//our server is ready to go
-//this lets u to communicate with server and client
-//we are ready to accept new connection
 app.use(express.static(publicPath));
-//default events that keep track of users when then connect  to server
-//and when they disconnect from server
-//greet the user when they make connection
 
 
-
-//io.on is used for connection event
-//we dont attacch any event other than this
 
 io.on("connection",(socket)=>{
 
-    //our custom events listeners are happen down below
-
-    //on=>listen the event
     
     socket.emit("newMessage",generate_message("Admin","Welcome to the chat app"));
 
     
     socket.broadcast.emit("newMessage",generate_message("Admin","New User Joined"));
     
-    // socket.on("createEmail",(newEmail)=>{
-    //     console.log("createEmail",newEmail);
-    // })
-
-
-
-    //socket =>this refers to individual socket as opposed to all of users connected to server
-    //web sockets=they are persistent technology
-    //meaning that client and server they both keep communicationn shell open
-    //if server shut down than client has no choice
-    //when i close browser server cant force to open the connection
     console.log("New User connected")
 
-
-
-
-    //if u wnt to set custom data 
-    // socket.emit("newEmail",{
-    //     from:"gaurav khurana",
-    //     text:"Hey what is going on",
-    //     createdAt:123
-    // });
-
-    //Emitting New Message
-    // socket.emit("newMessage",{
-    //   from:"gaurav KHurana",
-    //   text:"Helloooo",
-    //   createdAt:"1234"  
-    // })
-
-    //listening createMessageEvent
-    
-    //socket.emit->//emits the  event single connection
-    //io.emit->//emits the event to all connection
-
-    socket.on("createMessageEvent",function(message){
+    socket.on("createMessageEvent",(message,callback)=>{
         console.log("New message from Client ",message)
-io.emit("newMessage",generate_message(message.from,message.text))
-
-        //this event is emit for everybody except this socket
-        // socket.broadcast.emit("newMessage",{
-        //         from:message.from,
-        //         text:message.text,
-        //         createdAt:new Date().getTime()
-        //     })
+  io.emit("newMessage",generate_message(message.from,message.text))
+        callback("This is form the server");
     })
 
  
@@ -99,20 +39,9 @@ io.emit("newMessage",generate_message(message.from,message.text))
         console.log("User was disconnected")
     })
 })
-//it listen for new connection 
-//meaning that when client connects to server
-//and do something 
-//when it gets registerd
-
-//io.on let u to register the event listener
-//we listen that event and do something when this event has happened
 server.listen(3000,()=>{
     console.log(`server is up on port ${PORT}`);
 })
 
 
 
-//just like express ,whick makes easy to make http server
-//socket io makes(dead simple) to set up server that suppotts easy to set up web sockets 
-//socket io has backend and frontend library
- 
