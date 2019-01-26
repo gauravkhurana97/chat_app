230d00d8c7de0a3e33eb45035a08caaf8af0bc8d
gauravkhurana <gauravkhurana154@gmail.com>
2019-01-26 16:55:03 +0530

    add form for messages and show incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -6,18 +6,38 @@ socket.on("connect",function(){
 });
 
 
+// socket.on("newMessage",function(message){
+//     console.log("Got it",message)
+// })
+
 socket.on("newMessage",function(message){
     console.log("Got it",message)
+    var li = jQuery("<li></li>");
+    li.text(`${message.from}:${message.text}`);
+    jQuery("#messages").append(li);
+
 })
 
-//Acknowledgment  
-socket.emit("createMessageEvent",{
-    from:"frank",
-    text:"Hii" 
-},function(msg){
-    console.log(msg);
-})
+// //Acknowledgment  
+// socket.emit("createMessageEvent",{
+//     from:"frank",
+//     text:"Hii" 
+// },function(msg){
+//     console.log(msg);
+// })
 
 socket.on("disconnect",function(){
     console.log("Disconnected from server");
+})
+
+jQuery("#message_form").on("submit",function(e){
+    e.preventDefault();
+
+    socket.emit("createMessageEvent",{
+        from:"User",
+        text:jQuery('[name=message]').val()
+    },function(){
+        
+    }
+    )
 })
